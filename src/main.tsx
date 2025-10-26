eb228fda0daa32b5c9539b3814b2897cde9c6416
Sam May <sam@futurelab.solutions>
2025-10-26 12:59:50 +0000

    Remove unused achievements/notifications system

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 import { ErrorBoundary } from "react-error-boundary";
 import { Toaster } from "@/components/ui/sonner";
 import { AuthProvider } from '@/hooks/use-auth';
-import { AchievementsProvider } from '@/hooks/use-achievements-context';
 
 import App from './App.tsx'
 import { ErrorFallback } from './ErrorFallback.tsx'
@@ -16,16 +15,14 @@ createRoot(document.getElementById('root')!).render(
   <ErrorBoundary FallbackComponent={ErrorFallback}>
     <BrowserRouter>
       <AuthProvider>
-        <AchievementsProvider>
-          <App />
-          <Toaster 
-            position="bottom-center"
-            closeButton
-            richColors
-            expand={false}
-            duration={4000}
-          />
-        </AchievementsProvider>
+        <App />
+        <Toaster 
+          position="bottom-center"
+          closeButton
+          richColors
+          expand={false}
+          duration={4000}
+        />
       </AuthProvider>
     </BrowserRouter>
    </ErrorBoundary>
